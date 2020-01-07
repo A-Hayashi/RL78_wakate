18d741afbbf3feb224852250799d7cdae4a63578
dracen <a.hayashi0113@gmail.com>
2020-01-08 01:02:00 +0900

    4

---
+++ ex_4_4_Timer_CCRL/src/interrupt_handlers.c
@@ -0,0 +1,18 @@
+/***********************************************************************/
+/*                                                                     */
+/*  FILE        :interrupt_handlers.c                                  */
+/*  DATE        :                                                      */
+/*  DESCRIPTION :Interrupt Program                                     */
+/*  CPU TYPE    :                                                      */
+/*                                                                     */
+/*  NOTE:THIS IS A TYPICAL EXAMPLE.                                    */
+/*                                                                     */
+/***********************************************************************/
+#include "iodefine.h"
+
+extern unsigned char portValue;
+#pragma interrupt INT_TM00(vect=INTTM00)
+void INT_TM00 (void)
+{
+  portValue++;                         // カウントアップ
+}
---
+++ ex_4_4_Timer_CCRL/src/main.c
@@ -0,0 +1,31 @@
+/***********************************************************************/
+/*                                                                     */
+/*  FILE        :Main.c                                                */
+/*  DATE        :                                                      */
+/*  DESCRIPTION :Main Program                                          */
+/*  CPU TYPE    :                                                      */
+/*                                                                     */
+/*  NOTE:THIS IS A TYPICAL EXAMPLE.                                    */
+/*                                                                     */
+/***********************************************************************/
+#include "iodefine.h"
+#include "macrodriver.h"
+
+unsigned char portValue=0;
+void main(void);
+extern void HardwareSetup(void);
+
+void main(void)
+{
+  HardwareSetup();                     // ハードウェア設定
+  
+	P1=0xff;                             // P1全High
+	PM1=0x00;                            // P1全出力設定
+  
+	TS0=0x0001;                          // タイマ00カウント開始
+	
+  while(1)
+  {
+    P1=~portValue;                     // 割り込みカウンタの反転出力
+  }
+}
